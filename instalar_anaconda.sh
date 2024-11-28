6281eea09b3e53761a9b4619aa999d8c307788fb
Francesc Rodriguez Diaz <102602195+FranRodDiaz@users.noreply.github.com>
2024-11-28 11:34:20 +0100

    Update instalar_anaconda.sh

--- instalar_anaconda.sh
+++ instalar_anaconda.sh
@@ -5,7 +5,7 @@ sudo apt-get install net-tools -y
 curl -O https://repo.anaconda.com/archive/Anaconda3-2024.10-1-Linux-x86_64.sh
 bash Anaconda3-2024.10-1-Linux-x86_64.sh -b -p $HOME/anaconda3
 echo "/home/eps/anaconda3/bin" >> ~/.bashrc
-sudo chmod +x /home/eps/anaconda3/etc/profile.d/conda.sh
+#sudo chmod +x /home/eps/anaconda3/etc/profile.d/conda.sh
 source ~/.bashrc
 conda env create --name entornoIA2425 --file=entornoIA2425_MAC.yml
 conda activate entornoIA2425
