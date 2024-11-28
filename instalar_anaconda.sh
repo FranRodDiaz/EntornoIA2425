a529cddfe9e734853aa44d94f5a4b58c11957d20
Francesc Rodriguez Diaz <102602195+FranRodDiaz@users.noreply.github.com>
2024-11-28 10:25:31 +0100

    Archivos instalación entornoIA2425

---
+++ instalar_anaconda.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install openssh-server -y
+sudo apt-get install net-tools -y
+cd /tmp
+curl -O https://repo.anaconda.com/archive/Anaconda3-2024.10-1-Linux-x86_64.sh
+bash Anaconda3-2024.10-1-Linux-x86_64.sh -b -p $HOME/anaconda3
+source ~/.bashrc
+reset
+conda env create --name entornoIA2425 --file=entornoIA2425_MAC.yml
+conda activate entornoIA2425
+conda install anaconda-navigator -y
+#conda config --set auto_activate_base false
+
+#conda activate entornoIA2425
+#anaconda-navigator o jupyter-lab &
+#Abrir el jupyterlab normalmente
